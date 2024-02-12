3b0d9caabc436990044f86735070cbce963207f3
PartySlayer <120326157+PartySlayer@users.noreply.github.com>
2024-02-12 19:59:26 +0100

    Corretto typo
    la rt pubblica sarebbe stata associata alla subnet privata app az2 invece che alla subnet pubblica az2.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -67,7 +67,7 @@ resource "aws_route_table_association" "public_subnet_az1_route_table_associatio
 
 # associa "public subnet az2" to "public route table"
 resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id           = aws_subnet.private_app_subnet_az2.id
+  subnet_id           = aws_subnet.public_subnet_az2.id
   route_table_id      = aws_route_table.public_route_table.id
 }
 
